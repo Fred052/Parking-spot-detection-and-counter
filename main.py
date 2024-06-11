fe18eb7178341378e995b39bca2fa05df1b62e8e
Fred052 <ferid.suleymanzade05@gmail.com>
2024-06-12 00:11:59 +0300

    ProjectCompleted

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
 from util import get_parking_spots_bboxes, empty_or_not
+
+
+def calc_diff(im1, im2):
+    return np.abs(np.mean(im1) - np.mean(im2))
 
 mask = './mask_1920_1080.png'
 
@@ -15,6 +21,9 @@ connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
 spots = get_parking_spots_bboxes(connected_components)
 
 spots_status = [None for j in spots]
+diffs = [None for j in spots]
+
+previous_frame = None
 
 print(spots[0])
 
@@ -24,14 +33,32 @@ step = 30
 while ret:
     ret, frame = cap.read()
 
-    if frame_nmr % step == 0:
+    if frame_nmr % step == 0 and previous_frame is not None:
         for spot_indx, spot in enumerate(spots):
+            x1, y1, w, h = spot
+
+            spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
+
+            diffs[spot_indx] = calc_diff(spot_crop, previous_frame[y1:y1 + h, x1:x1 + w, :])
+
+        print([diffs[j] for j in np.argsort(diffs)][:: -1])
+
+    if frame_nmr % step == 0:
+        if previous_frame is None:
+            arr_ = range(len(spots))
+        else:
+            arr_ = [j for j in np.argsort(diffs) if diffs[j] / np.amax(diffs) > 0.4]
+        for spot_indx in arr_:
+            spot = spots[spot_indx]
             x1, y1, w, h = spot
 
             spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
 
             spot_status = empty_or_not(spot_crop)
             spots_status[spot_indx] = spot_status
+
+    if frame_nmr % step == 0:
+        previous_frame = frame.copy()
 
     for spot_indx, spot in enumerate(spots):
          spot_status = spots_status[spot_indx]
@@ -41,6 +68,10 @@ while ret:
             frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
          else:
             frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
+
+    cv2.rectangle(frame, (80, 20), (480, 80), (0,0,0), -1)
+    cv2.putText(frame, 'Bos Yerler: {} / {}'.format(str(sum(spots_status)), str(len(spots_status))), (100, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
     cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
     cv2.imshow('frame', frame)
